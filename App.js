824ca33f710a1054f27f24516b7f44d202b582ee
dominic <dominicdsaprabhu@gmail.com>
2024-03-05 20:56:35 +0530

    first

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className="flex-1 justify-center items-center bg-black">
+      <Text className="text-white">Dominic Khankichele</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
